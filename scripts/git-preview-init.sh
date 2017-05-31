2233f5ac37f2f6c8ce999f68f532d4bedde4d371
Aaron Tay <tay-aaron@dmm.com>
2017-05-31 18:54:48 +0900

    Added script to add git to bash prompt
    The current branch of the current repo will be displayed on the bash prompt terminal

---
+++ scripts/git-preview-init.sh
@@ -0,0 +1,7 @@
+# https://coderwall.com/p/fasnya/add-git-branch-name-to-bash-prompt
+# This will enable the current branch name of a git repo to be display in the terminal prompt
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+}
+
+export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
