b5f298baba7c818cce2673b48b2f864fcbca54b2
Aaron Tay <tay-aaron@dmm.com>
2017-05-31 18:58:16 +0900

    Updated comment (trivial)

--- scripts/git-preview-init.sh
+++ scripts/git-preview-init.sh
@@ -1,5 +1,7 @@
 # https://coderwall.com/p/fasnya/add-git-branch-name-to-bash-prompt
 # This will enable the current branch name of a git repo to be display in the terminal prompt
+# Simply copy or invoke this script on start-up (e.g. .bash_profile)
+
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
